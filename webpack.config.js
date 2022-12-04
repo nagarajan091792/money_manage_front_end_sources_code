91996c7de82eb516a96758427ac5f7f626f1758f
nagarajan091792 <nagarajan091792@gmail.com>
2022-12-05 01:51:20 +0530

    g

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+// module.exports = {
+//     resolve: {
+//         fallback: { url: require.resolve("url/") }
+//     },
+//     resolve: {
+//         fallback: { assert: require.resolve("assert/")  }
+//     },
+//     resolve: {
+//         fallback: { stream: require.resolve("stream-browserify")  }
+// },
+// resolve: {
+//     fallback: { https: require.resolve("https-browserify") }
+// },
+// resolve: {
+//     fallback: { http: require.resolve("stream-http") }
+// },
+
+// }
